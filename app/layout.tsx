e7f46cb5d46a02daf52937194750f03b533758c2
Musab Khadim <musabkhadim363@gmail.com>
2026-01-02 21:18:22 +0500

    don

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VGS5Z40CMQ"></script>
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-VGS5Z40CMQ');` }} />
+      </head>
       <body className={`${poppins.className} antialiased`}>
         <ClientLayout>{children}</ClientLayout>
       </body>
